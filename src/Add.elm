e60cbb61296e7bd58adeb4bb5b2d7b9cf253db48
bec ¯\_(ツ)_/¯ <benoit.chiquet@fretlink.com>
2021-10-21 21:20:39 +0200

    example release

--- src/Add.elm
+++ src/Add.elm
@@ -1,22 +1,31 @@
 module Add exposing (add, forTest)
 
+{-| This module exposes a simple add function, as well as internals for test.
+  
+@docs add
+
+`add` relies on an `internalAdd` function which is not exposed.
+
+However, `internalAdd` is wrapped in forTest:
+
+@docs forTest
+```
+forTest = ForTest { internalAdd = internalAdd }
+```
+
+see `tests/Spec.elm` to see how this is used.
+|-}
 import Internal exposing (ForTest(..))
 
-{-| This is exposed in the module and the package.
-@docs add |-}
-
+{-| This is exposed in the module and the package. |-}
 add : Int -> Int -> Int
 add = internalAdd
-
-{-| This is never exposed outside of this module
-@docs internalAdd |-}
 
 internalAdd : Int -> Int -> Int
 internalAdd = (+)
 
 {-| This is exposed in the module and the package,
 but since `ForTest` is not exposed by the package,
-it is unusable outside of the package.
-@docs forTest |-}
-
+it is unusable outside of the package. |-}
+forTest : ForTest {internalAdd : (Int -> Int -> Int)}
 forTest = ForTest { internalAdd = internalAdd }
