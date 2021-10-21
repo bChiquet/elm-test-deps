171e375c4773143d0a9f3ca90cb4f8086b1adc47
bec ¯\_(ツ)_/¯ <benoit.chiquet@fretlink.com>
2021-10-21 21:19:14 +0200

    add documentation

--- src/Add.elm
+++ src/Add.elm
@@ -2,10 +2,21 @@ module Add exposing (add, forTest)
 
 import Internal exposing (ForTest(..))
 
+{-| This is exposed in the module and the package.
+@docs add |-}
+
 add : Int -> Int -> Int
 add = internalAdd
 
+{-| This is never exposed outside of this module
+@docs internalAdd |-}
+
 internalAdd : Int -> Int -> Int
 internalAdd = (+)
+
+{-| This is exposed in the module and the package,
+but since `ForTest` is not exposed by the package,
+it is unusable outside of the package.
+@docs forTest |-}
 
 forTest = ForTest { internalAdd = internalAdd }
